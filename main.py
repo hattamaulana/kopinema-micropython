3d831c2d5810b45ae831ff61f27d8b9fa10270a6
HattaMaulana <mahatta.maulana@gmail.com>
2019-08-19 01:39:12 +0700

    Add Module websockets client & socketio client manually

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 from networking import Network
+from websocket import client
+import os
+
 
 net = Network()
 
 connection = net.is_connected()
+
+
+def tesSocketIo(url):
+    with client.connect(url) as socketio:
+        uname = os.uname()
+        name = '{sysname} {release} {version} {machine}'.format(
+            sysname=uname.sysname,
+            release=uname.release,
+            version=uname.version,
+            machine=uname.machine,
+        )
+
+        socketio.send(name)
+        print("> {}".format(name))
+
+        while True:
+            greeting = socketio.recv()
+            print("< {}".format(greeting))
